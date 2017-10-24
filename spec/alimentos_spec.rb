9d601c2c5e21a4fada5491cfbd43784f0d2ab189
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2017-10-24 20:06:23 +0100

    Creación de test en spec para comprobar el correcto almacenamiento de los datos de los alimentos en la clase alimentos

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -10,5 +10,45 @@ describe Alimentos do
     @alimentos_5 = Alimentos.new("Ternera", 21.1, 0.0, 6.3)
     @alimentos_6 = Alimentos.new("Pollo", 20.6, 0.0, 5.6)
   end
+  
+  describe "# almacenamiento de datos de un alimento" do
+        context "Existiendo los datos del alimento" do    
+            it "Se almacena correctamente el nombre del alimento" do
+                expect(@alimentos.alimento).to eq("Huevo frito")
+                expect(@alimentos_2.alimento).to eq("Leche Vaca")
+                expect(@alimentos_3.alimento).to eq("Yogurt")
+                expect(@alimentos_4.alimento).to eq("Cerdo") 
+                expect(@alimentos_5.alimento).to eq("Ternera") 
+                expect(@alimentos_6.alimento).to eq("Pollo") 
+            end
+        
+            it "Se almacena correctamente las proteinas del alimento" do
+                expect(@alimentos.proteinas).to eq(14.1)
+                expect(@alimentos_2.proteinas).to eq(3.3)
+                expect(@alimentos_3.proteinas).to eq(3.8)
+                expect(@alimentos_4.proteinas).to eq(21.5)
+                expect(@alimentos_5.proteinas).to eq(21.1)
+                expect(@alimentos_6.proteinas).to eq(20.6)
+            end
+        
+            it "Se almacena correctamente los glucidos del alimento" do
+                expect(@alimentos.glucidos).to eq(0.0) 
+                expect(@alimentos_2.glucidos).to eq(4.8)
+                expect(@alimentos_3.glucidos).to eq(4.9)
+                expect(@alimentos_4.glucidos).to eq(0.0)
+                expect(@alimentos_5.glucidos).to eq(0.0)
+                expect(@alimentos_6.glucidos).to eq(0.0)
+            end
+        
+            it "Se almacena correctamente las grasas del alimento" do
+                expect(@alimentos.grasas).to eq(19.5)
+                expect(@alimentos_2.grasas).to eq(3.2)
+                expect(@alimentos_3.grasas).to eq(3.8)
+                expect(@alimentos_4.grasas).to eq(6.3)
+                expect(@alimentos_5.grasas).to eq(6.3)
+                expect(@alimentos_6.grasas).to eq(5.6)
+            end
+        end
+    end
 
 end
