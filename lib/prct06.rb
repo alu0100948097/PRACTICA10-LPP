640f410afe572af6574f74f432c37f9c9481a5d4
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2017-11-09 17:35:45 +0000

    Creación de método initialize en la clase list

--- lib/prct06.rb
+++ lib/prct06.rb
@@ -1,6 +1,7 @@
 require "prct06/version"
 require "prct06/alimentos"
 require "prct06/nodo"
+require "prct06/lista"
 
 module Prct06
     # Your code goes here...
---
+++ lib/prct06/lista.rb
@@ -0,0 +1,15 @@
+require 'prct06/nodo'
+
+class List
+	
+	attr_reader :cola, :cabeza, :tam
+	
+	def initialize
+		
+	   @cola=nil
+	   @cabeza=nil
+	   @tam=0
+	   
+	end
+	
+end
