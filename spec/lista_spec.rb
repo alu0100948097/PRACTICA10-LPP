b427fa48efa60cd540574a32ef52273dfdc44c7c
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2017-11-09 17:37:54 +0000

    Creación de test para comprobar valor de atributos por defecto de la clase list

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -8,4 +8,22 @@ describe List do
         
     end
     
+    describe "# datos de una lista" do
+        
+        context "Si no se establece cola ni cabeza en la lista" do
+            it "Cola de la lista por defecto" do
+                expect(@lista.cola).to eq(nil)
+            end
+            
+            it "Cabeza de la lista por defecto" do
+                expect(@lista.cabeza).to eq(nil)
+            end
+            
+            it "Tamaño de la lista por defecto" do
+                expect(@lista.tam).to eq(0)
+            end
+        end
+        
+    end
+    
 end
