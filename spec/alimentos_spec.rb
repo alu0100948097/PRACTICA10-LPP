b2ebc0f89fd4d895ccf58a6e308e038f84c71078
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2017-11-09 16:53:35 +0000

    Cambio en la estructura de directorios

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -4,7 +4,7 @@ describe Alimentos do
 
   before :each do
     @alimentos = Alimentos.new("Huevo frito", 14.1, 0.0, 19.5)
-	@alimentos_2 = Alimentos.new("Leche Vaca", 3.3, 4.8, 3.2)
+    @alimentos_2 = Alimentos.new("Leche Vaca", 3.3, 4.8, 3.2)
     @alimentos_3 = Alimentos.new("Yogurt", 3.8, 4.9, 3.8)
     @alimentos_4 = Alimentos.new("Cerdo", 21.5, 0.0, 6.3)
     @alimentos_5 = Alimentos.new("Ternera", 21.1, 0.0, 6.3)
