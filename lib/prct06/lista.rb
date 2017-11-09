2176ee498ea637809fb7ce90b64298810cfbd9fe
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2017-11-09 17:41:34 +0000

    Creación de método introducir_en_cola en la clase list

--- lib/prct06/lista.rb
+++ lib/prct06/lista.rb
@@ -11,5 +11,22 @@ class List
 	   @tam=0
 	   
 	end
+    
+    def introducir_en_cola!(valor)
+        
+        if(@tam==0)
+            @cola=Node.new(valor,nil,nil)
+            @cabeza= cola
+            
+            else
+            @cola.prev=Node.new(valor,cola,nil)
+            @cola.prev.next= cola
+            @cola= cola.prev
+            
+        end
+        
+        @tam+=1
+        
+    end
 	
 end
