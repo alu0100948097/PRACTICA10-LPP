2aa4edceb9c5926ee93232e70415279fce956dd1
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2017-11-14 17:57:27 +0000

    Creando objetos de la clase G_alimentos en el spec en relación a la comparabilidad de las instancias de la clase Alimentos

---
+++ spec/comparable_spec.rb
@@ -0,0 +1,14 @@
+require 'prct06'
+
+describe G_alimentos do
+    
+    before :each do
+        @g_alimentos = G_alimentos.new("Huevo frito", 14.1, 0.0, 19.5, "Huevos, lácteos y helados")
+        @g_alimentos_2 = G_alimentos.new("Leche Vaca", 3.3, 4.8, 3.2, "Huevos, lácteos y helados")
+        @g_alimentos_3 = G_alimentos.new("Yogurt", 3.8, 4.9, 3.8, "Huevos, lácteos y helados")
+        @g_alimentos_4 = G_alimentos.new("Cerdo", 21.5, 0.0, 6.3, "Carnes y derivados")
+        @g_alimentos_5 = G_alimentos.new("Ternera", 21.1, 0.0, 6.3, "Carnes y derivados")
+        @g_alimentos_6 = G_alimentos.new("Pollo", 20.6, 0.0, 5.6, "Carnes y derivados")
+    end
+    
+end
