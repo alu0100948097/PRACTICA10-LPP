25aeb2cbe111643ff69341d62361cd758aff425c
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2017-11-21 12:07:50 +0000

    Creación de los tests para comprobar que se almacenan correctamente los valores de los atributos de la clase

--- spec/funcional_spec.rb
+++ spec/funcional_spec.rb
@@ -11,8 +11,39 @@ describe Alimentos do
     glucosa=[]
     glucosa<<[4.9 , 5.3 , 5.9 , 6.7 , 7.2 , 7.6 , 8.0 , 8.2 , 8.2 , 8.4 , 8.3 , 8.3 , 8.0 , 7.5 , 7.1 , 6.8 , 6.8 , 6.9 , 6.8 , 6.3 , 6.2 , 6.3 , 6.2 , 6.3 , 6.1]
     glucosa<<[6.3 , 5.4 , 5.6 , 5.7 , 6.5 , 7.4 , 7.9 , 7.4 , 7.7 , 7.9 , 7.9 , 7.8 , 7.8 , 7.8 , 8.0 , 8.5 , 9.4 , 10.8 , 10.5 , 9.1 , 8.9 , 8.3 , 7.7 , 7.6 , 7.5]
-    @glucosa = Alimentos.new("Glucosa", 0, 0, 0)
+    @glucosa = Alimentos.new("Glucosa", 0, 99.8, 0)
     @glucosa.g=glucosa 
+  end
+  
+  describe "# almacenamiento de datos de un alimento" do
+      
+      context "Existiendo los datos del alimento" do
+          
+          it "Se almacena correctamente el nombre del alimento" do
+              expect(@yogurt.alimento).to eq("Yogurt")
+              expect(@glucosa.alimento).to eq("Glucosa")
+          end
+          
+          it "Se almacena correctamente las proteinas del alimento" do
+              expect(@yogurt.proteinas).to eq(3.8)
+              expect(@glucosa.proteinas).to eq(0)
+          end
+          
+          it "Se almacena correctamente los glucidos del alimento" do
+              expect(@yogurt.glucidos).to eq(4.9)
+              expect(@glucosa.glucidos).to eq(99.8)
+          end
+          
+          it "Se almacena correctamente las grasas del alimento" do
+              expect(@yogurt.grasas).to eq(3.8)
+              expect(@glucosa.grasas).to eq(0)
+          end
+          
+          it "Se almacena correctamente las mediciones de los individuos" do
+              expect(@yogurt.g).to eq([[6.1 , 6.6 , 6.3 , 6.3 , 6.1 , 6.9 , 6.8 , 6.5 , 6.4 , 6.9 , 6.8 , 6.5 , 6.3 , 6.2 , 6.7 , 6.2 , 5.9 , 5.8 , 5.8 , 5.8 , 5.8 , 5.8 , 5.9 , 6.2 , 6.4],[4.9 , 4.9 , 5.2 , 5.8 , 6.5 , 7.0 , 7.2 , 7.3 , 7.3 , 6.6 , 6.2 , 6.1 , 6.0 , 6.1 , 5.9 , 5.9 , 5.9 , 5.9 , 5.8 , 5.8 , 5.5 , 5.5 , 5.6 , 5.9 , 5.9]])
+              expect(@glucosa.g).to eq([[4.9 , 5.3 , 5.9 , 6.7 , 7.2 , 7.6 , 8.0 , 8.2 , 8.2 , 8.4 , 8.3 , 8.3 , 8.0 , 7.5 , 7.1 , 6.8 , 6.8 , 6.9 , 6.8 , 6.3 , 6.2 , 6.3 , 6.2 , 6.3 , 6.1],[6.3 , 5.4 , 5.6 , 5.7 , 6.5 , 7.4 , 7.9 , 7.4 , 7.7 , 7.9 , 7.9 , 7.8 , 7.8 , 7.8 , 8.0 , 8.5 , 9.4 , 10.8 , 10.5 , 9.1 , 8.9 , 8.3 , 7.7 , 7.6 , 7.5]])
+          end
+      end
   end
 
 end
