bc17ba82f223d8b547090c6cf840a2eec47a4dd1
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2017-11-28 11:44:17 +0000

    Declaración del método ordenar_for que se añadira en la clase por defecto Array

--- lib/prct06/array.rb
+++ lib/prct06/array.rb
@@ -1,1 +1,28 @@
-
+# encoding: utf-8
+# Se definen nuevos métodos para la clase Array por defecto
+# Su desarrollo ha sido dirigido por pruebas (TDD).
+#
+# Author::    Sergio Ferrera de Diego  (mailto:sergioferrera1296@gmail.com)
+# Copyright:: Cretive Commons
+# License::   Distributes under the same terms as Ruby
+class Array
+    
+    # Método para ordenador por inserción un array mediante bucles for
+    def ordenar_for
+        ordenado=Array.new
+        ordenado << self[0]
+        for i in (1..self.length-1)
+            for j in (0..ordenado.length-1)
+                if self[i]>=ordenado[j]
+                    ordenado.insert(j,self[i])
+                    break
+                end
+                if j==ordenado.length-1
+                    ordenado << self[i]
+                end
+            end
+        end
+        ordenado
+    end
+    
+end
